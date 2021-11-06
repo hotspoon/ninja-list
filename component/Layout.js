7e258b80b89b600deceac7a9b01b6b8639862d2c
hotspoon <fariez18@outlook.com>
2021-11-06 23:29:20 +0700

    Initial commit

---
+++ component/Layout.js
@@ -0,0 +1,14 @@
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="content">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="about" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam porro impedit hic, ut rem nesciunt amet possimus quae fugit voluptates.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam porro impedit hic, ut rem nesciunt amet possimus quae fugit voluptates.</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
